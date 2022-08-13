c11e8f094289c6d458ff80605d146f4166fa3a37
Danilo Melo <danilomarquesdemelo@gmail.com>
2022-08-13 12:41:18 -0300

    Implementado CSS da versão mobile

---
+++ assets/scripts/main.js
@@ -0,0 +1,22 @@
+// const openSideMenu = document.querySelector(".open-side-menu").onclick = function(){
+//     document.documentElement.classList.add("active-side-menu", "tranparent-bg")
+// }
+
+// const closeSideMenu = document.querySelector(".close-side-menu").onclick = function(){
+//     document.documentElement.classList.remove("active-side-menu", "tranparent-bg")
+// }
+
+// const features = document.querySelector(".menu__features")
+// const company = document.querySelector(".company")
+// const featuresDropDown = document.querySelector(".menu__features-dropDown")
+
+
+
+
+// features.addEventListener("click", ()=>{
+//     if(featuresDropDown.style.display === "none"){
+//         featuresDropDown.style.display = "block"
+//     }else{
+//         featuresDropDown.style.display = "none"
+//     }
+// })
